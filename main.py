c6e3c841f177cb1d0cb98fa35e86afdd6f3885ea
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-01-31 20:26:31 -0600

    Tryagain.py
    I feel like Im getting lost

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-#REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+    score = 0
+    if len(list1) != len(list2):
+       print("Error")
+    else:
+        for i in range(0, len(list1)):
+             score += list1[i] * list2[i]
+             print(score)
 
 if __name__ == '__main__':
-   #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+   list1 = int(input()).split()
+   list2 = int(input()).split()
+   print(sum_of_products(list1, list2))
